fbbaf9738f7ec7a847936a7a622099507603b53a
Mégane Vilain <megane.vilain@gmail.com>
2025-05-25 22:16:35 +0200

    Add basic recycler view

--- app/src/main/java/com/example/booktracker/HomeFragment.kt
+++ app/src/main/java/com/example/booktracker/HomeFragment.kt
@@ -5,9 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
@@ -33,8 +34,15 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val dataset = arrayOf("January", "February", "March")
+        val customAdapter = CustomAdapter(dataset)
+
+
+        val view =  inflater.inflate(R.layout.fragment_home, container, false)
+        val recyclerView: RecyclerView = view.findViewById(R.id.recycler_view)
+        recyclerView.layoutManager = GridLayoutManager(context,2)
+        recyclerView.adapter = customAdapter
+        return view
     }
 
     companion object {
